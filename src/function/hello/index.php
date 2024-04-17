5efe320f9b4745e423fc3baa121002488e205bd4
ddesioeleva <ddesio@eleva.it>
2024-04-17 12:45:49 +0200

    Update function code and serverless.yml

--- src/function/hello/index.php
+++ src/function/hello/index.php
@@ -4,7 +4,6 @@ namespace App;
 
 use Bref\Event\Handler;
 use Bref\Context\Context;
-use Nyholm\Psr7\Response;
 
 require 'vendor/autoload.php';
 
@@ -13,27 +12,24 @@ class HelloHandler implements Handler
     /**
      * @param $event
      * @param Context|null $context
-     * @return Response
+     * @return array
      */
-    public function handle($event, ?Context $context): Response
+    public function handle($event, ?Context $context): array
     {
-        $attributes = $event->getAttributes();
-        $queryParams = $event->getQueryParams();
-        $parsedBody = $event->getParsedBody();
-
-        $message = [
-            "message" =>'Bref! Your function executed successfully!',
-            "context" => $context,
-            "input" => [
-                "attributes"=>$attributes,
-                "queryParams"=>$queryParams,
-                "parsedBody"=>$parsedBody
-            ]
+        return [
+            "statusCode"=>200,
+            "headers"=>[
+                'Access-Control-Allow-Origin'=> '*',
+                'Access-Control-Allow-Credentials'=> true,
+                'Access-Control-Allow-Headers'=> '*',
+            ],
+            "body"=>json_encode([
+                "message" =>'Bref! Your function executed successfully!',
+                "context" => $context,
+                "input" => $event
+            ])
         ];
 
-        $status = 200;
-
-        return new Response($status, ["Content-Type"=>"application/json"], json_encode($message));
     }
 }
 
